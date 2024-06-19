d238d90379e47af2e43361c0ad733b1a0a0148d2
unknown <faridnegm44@gmail.com>
2024-06-19 19:00:02 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $links = [
+        'users' => 'users/index.php',
+        'posts' => 'posts/index.php',
+    ]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container text-center">
+        <h2>Home Page</h2>
+
+        <ul>
+            <?php foreach($links as $name => $path): ?>
+                <li>
+                    <a href="<?php echo $path ?>"><?php echo $name ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ users/table.php
@@ -0,0 +1,35 @@
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>اسم المستخدم</th>
+            <th>البريد الإلكتروني</th>
+            <th>الصورة</th>
+            <th>تعديل</th>
+            <th>حذف</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>John Doe</td>
+            <td>johndoe@example.com</td>
+            <td><img src="user.png" alt="صورة المستخدم" width="20" height="20"></td>
+            <td><a href="#" class="btn btn-primary btn-sm">تعديل</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm">حذف</a></td>
+        </tr>
+        <tr>
+            <td>ali</td>
+            <td>ali@example.com</td>
+            <td><img src="user.png" alt="صورة المستخدم" width="20" height="20"></td>
+            <td><a href="#" class="btn btn-primary btn-sm">تعديل</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm">حذف</a></td>
+        </tr>
+        <tr>
+            <td>Peter Jones</td>
+            <td>peterjones@example.com</td>
+            <td><img src="user.png" alt="صورة المستخدم" width="20" height="20"></td>
+            <td><a href="#" class="btn btn-primary btn-sm">تعديل</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm">حذف</a></td>
+        </tr>
+    </tbody>
+</table>
+
